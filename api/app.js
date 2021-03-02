783893843d1d8a47490c3fc353fff4712162158a
antoniogambogi <62033319+antoniogambogi@users.noreply.github.com>
2021-03-02 19:06:29 -0300

    Adicionando conexão com o MongoDB

--- api/app.js
+++ api/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('API')
+mongoose.connect('mongodb://localhost:27017/celke', {useNewUrlParser: true,
+useUnifiedTopology: true}).then(() => {
+    console.log("Conexão com o BD MongoDB realizado com sucesso!");
+}).catch((err) => {
+    console.log("ERROR: Conexão com o BD MongoDB falhou!");
+})
+
+app.get('/home', function (req, res) {
+    res.send('API online');
 });
 
 app.listen(8080, function () {
